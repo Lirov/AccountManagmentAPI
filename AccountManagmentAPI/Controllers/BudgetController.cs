cefb5c9afb4831d26efe93a7ee312128a5ba367a
lirov <lirovink@gmail.com>
2024-02-15 20:50:45 +0200

    Adding Controllers

---
+++ AccountManagmentAPI/Controllers/BudgetController.cs
@@ -0,0 +1,104 @@
+﻿using AccountManagmentAPI.Models;
+using AccountManagmentAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AccountManagmentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class BudgetController : ControllerBase
+    {
+        private readonly IBudgetService _budgetService;
+
+        public BudgetController(IBudgetService budgetService)
+        {
+            _budgetService = budgetService;
+        }
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Budget>>> GetAllBudgets()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var budgets = await _budgetService.GetAllBudgetsAsync(userId);
+            return Ok(budgets);
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Budget>> GetBudget(Guid id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var budget = await _budgetService.GetBudgetByIdAsync(id, userId);
+
+            if (budget == null)
+            {
+                return NotFound();
+            }
+
+            return budget;
+        }
+
+
+        [HttpPost]
+        public async Task<ActionResult<Budget>> CreateBudget(Budget budget)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null)
+            {
+                return Unauthorized();
+            }
+
+            budget.UserId = userId;
+            var createdBudget = await _budgetService.CreateBudgetAsync(budget, userId);
+            return CreatedAtAction(nameof(GetBudget), new { id = createdBudget.BudgetId }, createdBudget);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateBudget(Guid id, Budget budget)
+        {
+            if (id != budget.BudgetId)
+            {
+                return BadRequest();
+            }
+
+            //var userId = User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            if (userId == null || budget.UserId != userId)
+            {
+                // Prevent updating a budget that does not belong to the current user
+                return Unauthorized();
+            }
+
+            try
+            {
+                await _budgetService.UpdateBudgetAsync(budget, userId);
+            }
+            catch (KeyNotFoundException)
+            {
+                return NotFound();
+            }
+
+            return NoContent();
+        }
+
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBudget(Guid id)
+        {
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            await _budgetService.DeleteBudgetAsync(id, userId);
+
+            return NoContent();
+        }
+    }
+}
---
+++ AccountManagmentAPI/Controllers/CategoryController.cs
@@ -0,0 +1,76 @@
+﻿using AccountManagmentAPI.Models;
+using AccountManagmentAPI.Repositories.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Security.Claims;
+
+namespace AccountManagmentAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class CategoryController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoryController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Category>>> GetAllCategories()
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var categories = await _categoryService.GetAllCategoriesAsync(userId);
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Category>> GetCategory(Guid id)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var category = await _categoryService.GetCategoryByIdAsync(id, userId);
+
+            if (category == null)
+            {
+                return NotFound();
+            }
+
+            return category;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<Category>> CreateCategory(Category category)
+        {
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            var createdCategory = await _categoryService.CreateCategoryAsync(category, userId);
+            return CreatedAtAction(nameof(GetCategory), new { id = createdCategory.CategoryId }, createdCategory);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateCategory(Guid id, Category category)
+        {
+            if (id != category.CategoryId)
+            {
+                return BadRequest();
+            }
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+            await _categoryService.UpdateCategoryAsync(category, userId);
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCategory(Guid id)
+        {
+
+            var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
+
+            await _categoryService.DeleteCategoryAsync(id, userId);
+
+            return NoContent();
+        }
+    }
+}
