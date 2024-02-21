a2f629c1a1f4271b29659447088ae889ad70c280
lirov <lirovink@gmail.com>
2024-02-21 18:09:29 +0200

    Deleted duplicated CORS configuration

--- AccountManagmentAPI/Program.cs
+++ AccountManagmentAPI/Program.cs
@@ -2,7 +2,6 @@ using AccountManagmentAPI.Models;
 using AccountManagmentAPI.Repositories.Interfaces;
 using AccountManagmentAPI.Repositories.Services;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
-using Microsoft.AspNetCore.CookiePolicy;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -53,16 +52,6 @@ builder.Services.AddCors(options =>
     });
 });
 
-builder.Services.AddCors(options =>
-{
-    options.AddDefaultPolicy(policyBuilder =>
-    {
-        policyBuilder
-        .WithOrigins(builder.Configuration.GetSection("AllowedOrigins").Get<string[]>())
-        .WithHeaders("Authorization", "origin", "accept", "content-type")
-        .WithMethods("GET", "POST", "PUT", "DELETE");
-    });
-});
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
